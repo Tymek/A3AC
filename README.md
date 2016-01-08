6b81c03560e35fa3df23dfce439ee33f81d6f338
Tymoteusz Czech <czech@t.pl>
2016-01-08 21:01:02 +0100

    v2.03

---
+++ README.md
@@ -0,0 +1,84 @@
+# Arma 3 Artillery Calculator
+_for manual fire without in-game Artillery Computer._
+
+Last builds:
+- web online: [a3ac.app.tymek.cz](http://a3ac.app.tymek.cz/)
+- android native: [apk download](http://a3ac.app.tymek.cz/apk/)
+
+## Instructions
+
+
+### Usage
+If you don't know how to manual fire, watch [this video tutorial](https://www.youtube.com/watch?v=v6m232CL1Aw).
+
+![screenshot](http://app.tymek.cz/a3ac/screenshot.png)
+
+1. Artillery type
+	- mortar (MK6 Mortar / Mobile Mortar Truck)
+	- MLRS (M5 Sandstorm)
+	- howitzer (2S9 Sochor / M4 Scorcher)
+2. Mode
+	- map coordinates (default)
+	- direct range
+3. Main input area
+	- X/Y coordinates &ndash; 4 digits per field!
+	- altitude / range in meters
+4. Adjustment
+	- available (clickable) when yellow
+	- in-game default key is F
+5. Elevation
+	- in-game PageUp/PageDown
+	- hold Shift for precise manipulation
+6. Direction
+	- azimuth with up to 2 decilal places
+7. Estimated round travel time
+	- in seconds
+
+
+__No "submit/calculate" button__ &ndash; live output triggered by every input change
+
+
+---
+
+### Build
+__You need to meet all standard requirements for Cordova__, or send files to build server.
+
+```
+cordova prepare
+cordova build
+```
+
+### Contribution
+Made on Foundation framework with AngularJS and some jQuery. Sorry for poorly structured and undocumented (except some comments in Polish) code. App was not made to be public at first. On the plus side, code is very short, with main js file ~150 lines.
+
+## Changelog
+Changes tracked from first public version (v2). Repository initialized on v2.03
+
+#### 2.03
+- viewpoint and zoom fix for mobile devices
+- minor hotfixes
+
+#### 2.02 RC
+- image background
+- font (Dezen Pro Regular)
+- custom android app icon
+
+#### 2.01 beta
+- dark colours
+- ETA (shell travel time) calculation fixed
+- web favicon
+
+### Scheduled changes
+
+#### 2.04
+- store last settings (battery type and calculation mode)
+- background align fix when showing keyboard
+
+#### 2.05
+- ETA in minutes and seconds
+- calculation near range boundry with ground elevation difference (NaN error)
+
+### Planned features
+- shell spread (at least approximate value)
+- indication if low angle fire is unavailable
+- modded artillery (if existst and requested)
---
+++ www/functions.js
@@ -0,0 +1,157 @@
+/*global angular, $*/
+
+Math.rad = function (deg) {
+	'use strict';
+	return deg * Math.PI / 180;
+};
+
+Math.deg = function (rad) {
+	'use strict';
+	return rad * 180 / Math.PI;
+};
+Math.floatRound = function (x, p) {
+	'use strict';
+	p = Math.pow(10, p);
+	return Math.round(x * p) / p;
+};
+
+Math.bearing = function (y, x) {
+	'use strict';
+	if (x <= 0 && y >= 0) {
+		if (x === 0) {
+			return 0;
+		}
+		return 450 - Math.deg(Math.atan2(y, x));
+	}
+	return 90 - Math.deg(Math.atan2(y, x));
+};
+
+Math.eta = function (v, x, fi) {
+	'use strict';
+	var g = 9.80665;
+	return Math.floatRound((x / (v * Math.cos(Math.rad(fi)))), 0);
+};
+
+Math.fi = function (v, x, y, low_angle, g) {
+	'use strict';
+	var v2 = v * v,
+		v4 = v2 * v2,
+		x2 = x * x;
+
+	if (low_angle === undefined) {
+		low_angle = false;
+	}
+	if (g === undefined || isNaN(y)) {
+		g = 9.80665;
+	}
+	if (y === undefined || isNaN(y)) {
+		y = 0;
+	}
+
+	if (!low_angle) {
+		return Math.atan((v2 + Math.sqrt(v4 - g * ((g * x2) + (2 * y * v2)))) / (g * x));
+	}
+	return Math.atan((v2 - Math.sqrt(v4 - g * ((g * x2) + (2 * y * v2)))) / (g * x));
+
+};
+
+angular.module('armaApp', []).controller('armaCalcController', ['$scope', '$location', '$filter', function ($scope, $location, $filter) {
+	'use strict';
+	var ctrl = this;
+	this.modes = "";
+	this.types = [
+		{
+			id: "mortar",
+			name: "MK6 Mortar",
+			modes: [[34, 499, 70], [139, 1998, 140], [284, 4078, 200]]
+		},
+		{
+			id: "mlrs",
+			name: "M5 Sandstorm",
+			modes: [[799, 4604, 212.5], [3918, 18418, 425], [7196, 41442, 637.5], [12792, 73674, 772.5]]
+		},
+		{
+			id: "howitzer",
+			name: "2S9 Sochor / M4 Scorcher",
+			modes: [[826, 2415, 153.9], [2059, 6021, 243], [5271, 15414, 388.8], [14644, 42818, 648], [22881, 6903, 810]]
+		}
+	];
+	this.pref = 0;
+	this.b = 0;
+	this.calc = function (p) { // p - preferowane ustawienie (mode)
+		var v = ctrl.type, // typ pojazdu
+			x = ctrl.d, // odległość
+			y = ctrl.a, // wysokość
+			i,
+			t,
+			fi,
+			c = ctrl;
+
+		if (p !== undefined) {
+			c.pref = p;
+		}
+
+		// Odległość
+		if (!ctrl.insight) {
+			x = Math.round(100 * Math.sqrt(((c.bx - c.tx) * (c.bx - c.tx)) + ((c.by - c.ty) * (c.by - c.ty)))) / 10;
+			c.d = x;
+			y = Math.round(10 * (c.tz - c.bz)) / 10;
+			c.a = y;
+
+			c.b = Math.floatRound(Math.bearing(c.ty - c.by, c.tx - c.bx), 2);
+			$('.bearing .range-slider').foundation('slider', 'set_value', Math.round(c.b));
+		} else {
+			x = c.d;
+			y = c.a;
+		}
+
+		// Typ pojazdu
+		for (i = 0; i < ctrl.types.length; i += 1) {
+			t = ctrl.types[i];
+			if (t.id === v) {
+				v = t.modes;
+				break;
+			}
+		}
+
+		if (v !== undefined) {
+			c.modes = "";
+			for (i = v.length - 1; i >= 0; i -= 1) {
+				t = v[i];
+				if (x >= t[0] && x <= t[1]) { // porównanie odległości
+					c.mode = i;
+					c.modes += (i).toString();
+				}
+			}
+			if (c.modes !== "") {
+				// console.log(c.mode + "\t " + c.pref);
+				if (c.modes.indexOf(c.pref.toString()) !== -1) {
+					c.mode = c.pref;
+				}
+				v = v[c.mode];
+				$('.vertical-range').foundation('slider', 'set_value', c.mode + 1);
+				$('.vertical-range').foundation('slider', 'set_value', c.mode + 1);
+				v = v[2]; // prędkość pocisku
+
+				fi = Math.floatRound(Math.deg(Math.fi(v, x, y)), 2);
+				$("#elvh").val(fi);
+				$("#etah").val(Math.eta(v, x, fi));
+
+				fi = Math.floatRound(Math.deg(Math.fi(v, x, y, true)), 2);
+				$("#elvl").val(fi);
+				$("#etal").val(Math.eta(v, x, fi));
+			}
+		}
+
+	};
+	$scope.Math = Math;
+	$scope.fixed = Number.toFixed;
+}]);
+
+$(window).load(function () {
+	'use strict';
+	$(document).foundation();
+	/*$('.range .range-slider').on('change.fndtn.slider', function () {
+		
+	});*/
+});
